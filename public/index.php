395844baaa8e3dc7c940a78b1d35e1e69a295572
ThomasGirard <thomasg.pro@live.fr>
2026-06-26 11:35:37 +0200

    fonction comptage par type ajoutée

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../src/Factory/ProduitFactory.php';
 
 $repository = new ProduitRepository();
 $lignes = $repository->findAll();
+$tabType = $repository->countByType();
 
 $total = 0;
 
@@ -14,7 +15,13 @@ foreach($lignes as $ligne) {
 
     echo $produit->getNom() . " - TTC : $ttc € - Port : $port € - Type : " . $produit->getType() . "<br>";
 
-    $total += $ttc + $port;
+    $total += $ttc + $port;    
 }
 echo "TOTAL : $total €";
+echo "<br><br>";
+
+echo "QUANTITE PAR TYPE DE PRODUIT <br> =============================<br>";
+foreach($tabType as $tab) {
+    echo $tab['type'] . " = " . $tab['total'] . "<br>";
+}
 ?>
--- src/Repository/ProduitRepository.php
+++ src/Repository/ProduitRepository.php
@@ -9,5 +9,12 @@ class ProduitRepository
        $stmt = $pdo->query('SELECT * FROM produit') ;
        return $stmt->fetchAll();
     }
+
+    public function countByType(): array
+    {
+        $pdo = Database:: getConnexion();
+        $stmt = $pdo->query('SELECT type, count(type) as total FROM produit GROUP BY (type)');
+        return $stmt->fetchAll();
+    }
 }
 ?>
