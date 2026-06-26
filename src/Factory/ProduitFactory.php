c6dd4f2c00cea17570156dd38098c3c2162a2308
ThomasGirard <thomasg.pro@live.fr>
2026-06-26 10:17:59 +0200

    ajout d'un type 'vinyl' dans la bss et le panier

--- src/Factory/ProduitFactory.php
+++ src/Factory/ProduitFactory.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . '/../Model/livre.php';
 require_once __DIR__ . '/../Model/ebook.php';
+require_once __DIR__ . '/../Model/vinyle.php';
 
 class ProduitFactory
 {
@@ -9,6 +10,7 @@ class ProduitFactory
         return match($ligne['type']) {
             'livre' => new Livre($ligne['nom'], $ligne['prix_ht']),
             'ebook' => new Ebook($ligne['nom'], $ligne['prix_ht']),
+            'vinyle' => new Vinyle($ligne['nom'], $ligne['prix_ht']),
             default => throw new Exception("Type inconnu : " . $ligne['type'])
         };
     }
---
+++ src/Model/vinyle.php
@@ -0,0 +1,16 @@
+<?php
+require_once __DIR__ . '/produit.php';
+
+class Vinyle extends Produit
+{
+    public function calculerPrixTTC(): float 
+    {
+        return $this->prixHT * 1.20;
+    }
+
+    public function getFraisDePort(): float
+    {
+        return 4.0;
+    }
+}
+?>
